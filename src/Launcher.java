99e0aa61a831d2b17a5c9072e47699f7a2b415c1
John Rey <adioskid@mail.ru>
2022-12-13 17:39:14 +0300

    MemoryJava

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,6 +1,6 @@
 public class Launcher extends Swap {
     public static void main(String[] args) {
-        swap(5,10);
+        swap(5, 10);
         System.out.println(x);
         System.out.println(y);
     }
