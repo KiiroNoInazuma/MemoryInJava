1dbd5dfac5f507ce374d20d47b9e82f8de0c7577
John Rey <adioskid@mail.ru>
2022-12-22 17:51:25 +0300

    MemoryJava

--- src/Task4.java
+++ src/Task4.java
@@ -1,9 +1,13 @@
 public class Task4 {
 
-    static int twoNum(int a, int b){
-        return 0;
+    static int twoNum(int a, int b) {
+        for (int i = 13; i <= 19; i++) {
+            if (a == i||b==i) return 19;
+        }
+        return a + b;
     }
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        System.out.println(twoNum(13, 2));
     }
 }
