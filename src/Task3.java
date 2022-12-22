ab2e2ce31d8785bcfa87404993a52cbeba4aee98
John Rey <adioskid@mail.ru>
2022-12-22 16:06:59 +0300

    MemoryJava

--- src/Task3.java
+++ src/Task3.java
@@ -1,4 +1,14 @@
 public class Task3 {
+    static boolean six(int a, int b) {
+        if (a == 6 || b == 6) {
+            return true;
+        } else if (Math.abs(a - b) == 6 || a + b == 6) {
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
+        System.out.println(six(4, 10));
     }
 }
